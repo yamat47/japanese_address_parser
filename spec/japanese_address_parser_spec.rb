4b1816694d0f8644aa39e6bf4156c5dbfda565c7
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2022-01-01 01:06:50 +0900

    簡単なテストを追加した。

--- spec/japanese_address_parser_spec.rb
+++ spec/japanese_address_parser_spec.rb
@@ -1,11 +1,26 @@
 # frozen_string_literal: true
 
 RSpec.describe JapaneseAddressParser do
-  it "has a version number" do
-    expect(JapaneseAddressParser::VERSION).not_to be nil
-  end
+  describe '.call' do
+    shared_examples '町丁目まで解析できる' do
+      it { is_expected.to be_a(JapaneseAddressParser::Models::Address) }
+      it { expect(subject.furigana).to eq(furigana) }
+    end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    subject { described_class.call(full_address) }
+
+    context '全角英数字が含まれるとき' do
+      let(:full_address) { '東京都港区芝公園４ー２ー８' }
+      let(:furigana) { 'トウキョウトミナトクシバコウエン 4' }
+
+      it_behaves_like '町丁目まで解析できる'
+    end
+
+    context '英数字+「丁目」という表記のとき' do
+      let(:full_address) { '東京都港区芝公園4丁目2-8' }
+      let(:furigana) { 'トウキョウトミナトクシバコウエン 4' }
+
+      it_behaves_like '町丁目まで解析できる'
+    end
   end
 end
