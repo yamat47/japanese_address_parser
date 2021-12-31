9f2fc83e331ffeea83be1243b54566d38f6fd659
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2022-01-01 00:34:37 +0900

    住所を表すクラスを追加した。

--- lib/japanese_address_parser/prefecture.rb
+++ lib/japanese_address_parser/prefecture.rb
@@ -3,42 +3,44 @@
 require 'csv'
 require_relative './city'
 
-class Prefecture
-  attr_reader :code, :name, :name_kana, :name_romaji
+module JapaneseAddressParser
+  class Prefecture
+    attr_reader :code, :name, :name_kana, :name_romaji
 
-  class << self
-    def all
-      CSV.table('./parsed_data/prefectures.csv').map do |prefecture|
-        new(
-          code: prefecture[:code],
-          name: prefecture[:name],
-          name_kana: prefecture[:name_kana],
-          name_romaji: prefecture[:name_romaji]
-        )
+    class << self
+      def all
+        CSV.table('lib/japanese_address_parser/data/prefectures.csv').map do |prefecture|
+          new(
+            code: prefecture[:code],
+            name: prefecture[:name],
+            name_kana: prefecture[:name_kana],
+            name_romaji: prefecture[:name_romaji]
+          )
+        end
       end
     end
-  end
 
-  def initialize(code:, name:, name_kana:, name_romaji:)
-    @code = code
-    @name = name
-    @name_kana = name_kana
-    @name_romaji = name_romaji
-  end
+    def initialize(code:, name:, name_kana:, name_romaji:)
+      @code = code
+      @name = name
+      @name_kana = name_kana
+      @name_romaji = name_romaji
+    end
 
-  def formatted_code
-    format("%02<number>d", number: code)
-  end
+    def formatted_code
+      format("%02<number>d", number: code)
+    end
 
-  def cities
-    CSV.table("./parsed_data/#{formatted_code}.csv").map do |city|
-      City.new(
-        code: city[:code],
-        prefecture_code: city[:prefecture_code],
-        name: city[:name],
-        name_kana: city[:name_kana],
-        name_romaji: city[:name_romaji]
-      )
+    def cities
+      CSV.table("lib/japanese_address_parser/data/#{formatted_code}.csv").map do |city|
+        City.new(
+          code: city[:code],
+          prefecture_code: city[:prefecture_code],
+          name: city[:name],
+          name_kana: city[:name_kana],
+          name_romaji: city[:name_romaji]
+        )
+      end
     end
   end
 end
--- lib/japanese_address_parser/town.rb
+++ lib/japanese_address_parser/town.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
-class Town
-  attr_reader :name, :name_kana, :name_romaji, :nickname, :latitude, :longitude
+module JapaneseAddressParser
+  class Town
+    attr_reader :name, :name_kana, :name_romaji, :nickname, :latitude, :longitude
 
-  def initialize(name:, name_kana:, name_romaji:, nickname:, latitude:, longitude:)
-    @name = name
-    @name_kana = name_kana
-    @name_romaji = name_romaji
-    @nickname = nickname
-    @latitude = latitude
-    @longitude = longitude
+    def initialize(name:, name_kana:, name_romaji:, nickname:, latitude:, longitude:)
+      @name = name
+      @name_kana = name_kana
+      @name_romaji = name_romaji
+      @nickname = nickname
+      @latitude = latitude
+      @longitude = longitude
+    end
   end
 end
