a64ee6781af0ebf2266d826e90510f28860a3bbf
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2022-01-01 00:42:06 +0900

    ディレクトリ構造を調整した。

--- lib/japanese_address_parser.rb
+++ lib/japanese_address_parser.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require_relative "japanese_address_parser/address"
 require_relative "japanese_address_parser/version"
+require_relative "japanese_address_parser/models/address"
 
 module JapaneseAddressParser
   module_function
 
   def call(full_address)
-    Address.new(full_address)
+    Models::Address.new(full_address)
   end
 end
---
+++ lib/japanese_address_parser/models/town.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module JapaneseAddressParser
+  module Models
+    class Town
+      attr_reader :name, :name_kana, :name_romaji, :nickname, :latitude, :longitude
+
+      def initialize(name:, name_kana:, name_romaji:, nickname:, latitude:, longitude:)
+        @name = name
+        @name_kana = name_kana
+        @name_romaji = name_romaji
+        @nickname = nickname
+        @latitude = latitude
+        @longitude = longitude
+      end
+    end
+  end
+end
