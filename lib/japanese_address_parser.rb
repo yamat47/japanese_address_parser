a7d7a8b2934b42490ff925672835a4c7b8debb09
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2021-12-31 23:37:20 +0900

    コマンド: `bundle gem japanese_address_parser --coc --mit --test=rspec --ci=github --linter=rubocop`

---
+++ lib/japanese_address_parser.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "japanese_address_parser/version"
+
+module JapaneseAddressParser
+  class Error < StandardError; end
+  # Your code goes here...
+end
