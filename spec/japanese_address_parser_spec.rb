81c1e0ac1ace5ce95c010c17805bb907dbb1f827
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2022-01-08 12:34:52 +0900

    町域が含まれていないときに、市区町村に含まれる最初の町域がヒットしてしまっていた。

--- spec/japanese_address_parser_spec.rb
+++ spec/japanese_address_parser_spec.rb
@@ -6,6 +6,18 @@ require 'yaml'
   describe '.call' do
     subject(:parsed_address) { described_class.call(full_address) }
 
+    context '町域が含まれていないとき' do
+      let(:full_address) { '東京都北区' }
+
+      it '市区町村まで解析できる' do
+        expect(parsed_address).to(be_a(::JapaneseAddressParser::Models::Address))
+        expect(parsed_address.prefecture).to(be_a(::JapaneseAddressParser::Models::Prefecture))
+        expect(parsed_address.city).to(be_a(::JapaneseAddressParser::Models::City))
+        expect(parsed_address.town).to(be_nil)
+        expect(parsed_address.furigana).to(eq('トウキョウトキタク'))
+      end
+    end
+
     ::YAML.load_file('spec/addresses.yml').each do |address|
       context "#{address['full_address']}のとき" do
         let(:full_address) { address['full_address'] }
