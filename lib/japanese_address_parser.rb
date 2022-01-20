8c532a3bb7522a53e2a2f150b9988bc17f718c8b
Junya Ishihara <junya@champierre.com>
2022-01-20 22:55:45 +0900

    https://github.com/geolonia/normalize-japanese-addresses を使って住所を正規化する。

--- lib/japanese_address_parser.rb
+++ lib/japanese_address_parser.rb
@@ -2,11 +2,16 @@
 
 require_relative 'japanese_address_parser/address_parser'
 require_relative 'japanese_address_parser/version'
+require_relative 'japanese_address_parser/normalize_japanese_addresses_schmoozer'
 
 module JapaneseAddressParser
   module_function
 
   def call(full_address)
-    ::JapaneseAddressParser::AddressParser.call(full_address)
+    # https://github.com/geolonia/normalize-japanese-addresses を使って住所を正規化する。
+    normalize_japanese_addresses = ::JapaneseAddressParser::NormalizeJapaneseAddressesSchmoozer.new(__dir__)
+    result = normalize_japanese_addresses.normalize(full_address)
+
+    ::JapaneseAddressParser::AddressParser.call("#{result['pref']}#{result['city']}#{result['town']}#{result['addr']}")
   end
 end
---
+++ lib/japanese_address_parser/normalize_japanese_addresses_schmoozer.rb
@@ -0,0 +1,8 @@
+require 'schmooze'
+
+module JapaneseAddressParser
+  class NormalizeJapaneseAddressesSchmoozer < Schmooze::Base
+    dependencies normalize_japanese_addresses: '@geolonia/normalize-japanese-addresses'
+    method :normalize, "normalize_japanese_addresses.normalize"
+  end
+end
