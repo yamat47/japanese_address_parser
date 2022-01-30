f4c07a946dad53710883f6e96afc4e36df4089f1
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2022-01-30 22:36:04 +0900

    RuboCopの指摘に対応した。

--- lib/japanese_address_parser.rb
+++ lib/japanese_address_parser.rb
@@ -5,13 +5,14 @@ require_relative 'japanese_address_parser/version'
 require_relative 'japanese_address_parser/normalize_japanese_addresses_schmoozer'
 
 module JapaneseAddressParser
-  JS_PACKAGE_PATH = File.expand_path('../js', __dir__)
+  JS_PACKAGE_PATH = ::File.expand_path('../js', __dir__)
+  public_constant :JS_PACKAGE_PATH
 
   module_function
 
   def call(full_address)
     # https://github.com/geolonia/normalize-japanese-addresses を使って住所を正規化する。
-    normalize_japanese_addresses = ::JapaneseAddressParser::NormalizeJapaneseAddressesSchmoozer.new(JS_PACKAGE_PATH)
+    normalize_japanese_addresses = ::JapaneseAddressParser::NormalizeJapaneseAddressesSchmoozer.new(::JapaneseAddressParser::JS_PACKAGE_PATH)
     result = normalize_japanese_addresses.normalize(full_address)
 
     ::JapaneseAddressParser::AddressParser.call("#{result['pref']}#{result['city']}#{result['town']}#{result['addr']}")
--- lib/japanese_address_parser/normalize_japanese_addresses_schmoozer.rb
+++ lib/japanese_address_parser/normalize_japanese_addresses_schmoozer.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'schmooze'
 
 module JapaneseAddressParser
-  class NormalizeJapaneseAddressesSchmoozer < Schmooze::Base
+  class NormalizeJapaneseAddressesSchmoozer < ::Schmooze::Base
     dependencies normalize_japanese_addresses: '@geolonia/normalize-japanese-addresses'
-    method :normalize, "normalize_japanese_addresses.normalize"
+    method :normalize, 'normalize_japanese_addresses.normalize'
   end
 end
